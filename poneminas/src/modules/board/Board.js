ac59d9ba77682ee48b890abc81d0acfe3f763064
David Narvaez <dnarvaez27@outlook.com>
2018-10-03 13:45:04 -0500

    Game setup

--- poneminas/src/modules/board/Board.js
+++ poneminas/src/modules/board/Board.js
@@ -1,31 +1,62 @@
 import React, { Component } from "react";
-import Box from "../box/Box";
 import "./Board.css";
+import Box, { constants } from "../box/Box";
 
 export default class Board extends Component {
   constructor( props ) {
     super( props );
     this.state = {
       dimensions: { rows: 5, cols: 5 },
-      numBombs: 2
+      numBombs: 3
     };
+    this.cells = [];
   }
 
-  // See below for more details on randint function:
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  getRandomInt( min, max ) {
+  static getRandomInt( min, max ) {
     min = Math.ceil( min );
     max = Math.floor( max );
-    return Math.floor( Math.random() * (max - min) ) + min; //The maximum is exclusive and the minimum is inclusive
+    return Math.floor( Math.random() * (max - min) ) + min; // [min, max)
+  }
+
+  initCells() {
+    for ( let i = 0; i < this.state.dimensions.rows; i++ ) {
+      this.cells.push( [] );
+      for ( let j = 0; j < this.state.dimensions.cols; j++ ) {
+        this.cells[ i ][ j ] = { state: 0, row: i, col: j };
+      }
+    }
+  }
+
+  placeBombs() {
+    let freeCells = this.cells.flat();
+
+    let updateNeighbors = ( r, c ) => {
+      for ( let i = Math.max( 0, r - 1 ); i <= Math.min( r + 1, this.state.dimensions.rows - 1 ); i++ ) {
+        for ( let j = Math.max( 0, c - 1 ); j <= Math.min( c + 1, this.state.dimensions.cols -1 ); j++ ) {
+          if ( !(i === r && j === c) && !isNaN( this.cells[ i ][ j ].state ) ) {
+            this.cells[ i ][ j ].state++;
+          }
+        }
+      }
+    };
+
+    for ( let i = 0; i < this.state.numBombs; i++ ) {
+      let index = Board.getRandomInt( 0, freeCells.length );
+      let cellBomb = freeCells[ index ];
+      cellBomb.state = constants.BOMB;
+      updateNeighbors( cellBomb.row, cellBomb.col );
+      freeCells.splice( index, 1 );
+    }
   }
 
   render() {
+    this.initCells();
+    this.placeBombs();
+
     return (
       <div id="board-container">
         <div className="grid-container" style={{ "gridTemplateColumns": `repeat(${this.state.dimensions.cols}, 1fr)` }}>
-          {new Array( this.state.dimensions.rows * this.state.dimensions.cols )
-            .fill( null )
-            .map( ( e, i ) => <Box key={i}/> )}
+          {this.cells.flat().map( ( c, i ) => <Box key={i} state={c.state} row={c.row} col={c.col}/> )}
         </div>
       </div>
     );
--- poneminas/src/modules/box/Box.js
+++ poneminas/src/modules/box/Box.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import "./Box.css";
+
+export const constants = {
+  "BOMB": "-"
+};
 
 export default class Box extends Component {
   render() {
     return (
-      <button className="grid-item"/>
+      <button className="grid-item">{this.props.state === 0 ? "" : this.props.state}</button>
     );
   }
 }
+
+Box.propTypes = {
+  state: PropTypes.any.isRequired,
+  row: PropTypes.number.isRequired,
+  col: PropTypes.number.isRequired,
+};
