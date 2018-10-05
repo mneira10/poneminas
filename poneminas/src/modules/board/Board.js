fdce85921f1d9af1a4636c35cdbd104f651ae504
David Narvaez <dnarvaez27@outlook.com>
2018-10-05 01:47:15 -0500

    Minesweeper game. Fix flags onLost

--- poneminas/src/modules/board/Board.js
+++ poneminas/src/modules/board/Board.js
@@ -6,10 +6,19 @@ export default class Board extends Component {
   constructor( props ) {
     super( props );
     this.state = {
-      dimensions: { rows: 20, cols: 20 },
-      numBombs: 100
+      dimensions: { rows: 10, cols: 10 },
+      numBombs: 30
     };
     this.cells = [];
+    this.logicCells = [];
+    this.firstClick = false;
+    this.lost = false;
+    this.bombs = [];
+
+    this.boxClickHandle = this.boxClickHandle.bind( this );
+    this.showBoard = this.showBoard.bind( this );
+
+    this.initGame();
   }
 
   static getRandomInt( min, max ) {
@@ -18,23 +27,25 @@ export default class Board extends Component {
     return Math.floor( Math.random() * (max - min) ) + min; // [min, max)
   }
 
-  initCells() {
+  get_initial_state() {
+    let c = [];
     for ( let i = 0; i < this.state.dimensions.rows; i++ ) {
-      this.cells.push( [] );
+      c.push( [] );
       for ( let j = 0; j < this.state.dimensions.cols; j++ ) {
-        this.cells[ i ][ j ] = { state: 0, row: i, col: j };
+        c[ i ][ j ] = { state: 0, row: i, col: j };
       }
     }
+    return c;
   }
 
-  placeBombs() {
-    let freeCells = this.cells.flat();
+  placeBombs( c1 ) {
+    let freeCells = c1.flat();
 
     let updateNeighbors = ( r, c ) => {
       for ( let i = Math.max( 0, r - 1 ); i <= Math.min( r + 1, this.state.dimensions.rows - 1 ); i++ ) {
-        for ( let j = Math.max( 0, c - 1 ); j <= Math.min( c + 1, this.state.dimensions.cols -1 ); j++ ) {
-          if ( !(i === r && j === c) && !isNaN( this.cells[ i ][ j ].state ) ) {
-            this.cells[ i ][ j ].state++;
+        for ( let j = Math.max( 0, c - 1 ); j <= Math.min( c + 1, this.state.dimensions.cols - 1 ); j++ ) {
+          if ( !(i === r && j === c) && !isNaN( c1[ i ][ j ].state ) ) {
+            c1[ i ][ j ].state++;
           }
         }
       }
@@ -44,19 +55,123 @@ export default class Board extends Component {
       let index = Board.getRandomInt( 0, freeCells.length );
       let cellBomb = freeCells[ index ];
       cellBomb.state = constants.BOMB;
+      this.bombs.push( [ cellBomb.row, cellBomb.col ] );
       updateNeighbors( cellBomb.row, cellBomb.col );
       freeCells.splice( index, 1 );
     }
   }
 
-  render() {
-    this.initCells();
-    this.placeBombs();
+  get_neighbours( r, c ) {
+    let neighbours = [];
+    for ( let i = Math.max( 0, r - 1 ); i <= Math.min( r + 1, this.state.dimensions.rows - 1 ); i++ ) {
+      for ( let j = Math.max( 0, c - 1 ); j <= Math.min( c + 1, this.state.dimensions.cols - 1 ); j++ ) {
+        neighbours.push( this.cells[ i ][ j ] );
+      }
+    }
+    return neighbours;
+  }
 
+  floodFill( r, c, max ) {
+    if ( max !== 0 && !this.cells[ r ][ c ].current.show() && !isNaN( this.cells[ r ][ c ].current.props.state ) ) {
+      this.cells[ r ][ c ].current.state.show = true;
+      let neighs = this.get_neighbours( r, c );
+      neighs.forEach( ( n ) => {
+        n = n.current;
+        if ( this.cells[ n.props.row ][ n.props.col ].current.props.state === 0 ) {
+          this.floodFillZero( n.props.row, n.props.col );
+        }
+        else {
+          this.floodFill( n.props.row, n.props.col, max - 1 );
+        }
+      } );
+    }
+  }
+
+  floodFillZero( r, c ) {
+    if ( !this.cells[ r ][ c ].current.show() && !isNaN( this.cells[ r ][ c ].current.props.state ) ) {
+      this.cells[ r ][ c ].current.state.show = true;
+      if ( this.cells[ r ][ c ].current.props.state === 0 ) {
+        let neighs = this.get_neighbours( r, c );
+        neighs.forEach( ( n ) => {
+          n = n.current;
+          this.floodFillZero( n.props.row, n.props.col );
+        } );
+      }
+    }
+  }
+
+  boxClickHandle( r, c, state ) {
+    if ( !this.lost ) {
+      if ( !this.cells[ r ][ c ].current.show() ) {
+        this.cells[ r ][ c ].current.show( true );
+        if ( !this.firstClick ) {
+          this.floodFill( r, c, 3 ); //TODO: 3 as depth
+          this.firstClick = true;
+          this.forceUpdate();
+        }
+        else if ( state === constants.BOMB ) {
+          this.lost = true;
+          this.showBoard();
+        }
+        else if ( state === 0 ) {
+          this.floodFillZero( r, c );
+          this.forceUpdate();
+        }
+      }
+      else {
+        this.showElse( r, c );
+      }
+    }
+  }
+
+  initGame() {
+    this.logicCells = this.get_initial_state();
+    this.placeBombs( this.logicCells );
+  }
+
+  showBoard() {
+    for ( let i = 0; i < this.cells.length; i++ ) {
+      for ( let j = 0; j < this.cells[ i ].length; j++ ) {
+        this.cells[ i ][ j ].current.state.show = true;
+      }
+    }
+    this.forceUpdate();
+  }
+
+  showElse( r, c ) {
+    let ne = this.get_neighbours( r, c );
+    if ( ne.filter( i => i.current.state.flag !== 0 ).length >= this.cells[ r ][ c ].current.props.state ) {
+      ne.forEach( i => {
+        if ( i.current.state.flag === 0 ) {
+          i.current.state.show = true;
+          if ( i.current.props.state === constants.BOMB ) {
+            this.lost = true;
+          }
+        }
+      } );
+      this.forceUpdate();
+    }
+  }
+
+  render() {
+    this.cells = [];
     return (
       <div id="board-container">
         <div className="grid-container" style={{ "gridTemplateColumns": `repeat(${this.state.dimensions.cols}, 1fr)` }}>
-          {this.cells.flat().map( ( c, i ) => <Box key={i} state={c.state} row={c.row} col={c.col}/> )}
+          {this.logicCells.flat().map( ( c, i ) => {
+            if ( !this.cells[ c.row ] ) {
+              this.cells.push( [] );
+            }
+            this.cells[ c.row ].push( React.createRef() );
+            return (
+              <Box key={i}
+                state={c.state}
+                row={c.row}
+                col={c.col}
+                ref={this.cells[ c.row ][ c.col ]}
+                boxClickHandle={this.boxClickHandle}/>
+            );
+          } )}
         </div>
       </div>
     );
--- poneminas/src/modules/box/Box.js
+++ poneminas/src/modules/box/Box.js
@@ -7,9 +7,55 @@ export const constants = {
 };
 
 export default class Box extends Component {
+
+  constructor( props ) {
+    super( props );
+    this.handleClick = this.handleClick.bind( this );
+    this.contextHandleClick = this.contextHandleClick.bind( this );
+    this.state = { show: false, flag: 0 };
+  }
+
+  show( s, c ) {
+    if ( s ) {
+      this.setState( { show: s }, c );
+    }
+    else {
+      return this.state.show;
+    }
+  }
+
+  handleClick() {
+    if ( this.state.flag === 0 ) {
+      this.props.boxClickHandle( this.props.row, this.props.col, this.props.state );
+    }
+  }
+
+  contextHandleClick( e ) {
+    if ( !this.state.show ) {
+      this.setState( { flag: (this.state.flag + 1) % 3 } );
+    }
+    e.preventDefault();
+  }
+
   render() {
+    let box_state = this.props.state;
+    if ( this.props.state === constants.BOMB ) {
+      box_state = <i className="fas fa-bomb"/>;
+    }
+    if ( this.state.flag === 1 ) {
+      box_state = <i className="fas fa-flag"/>;
+    }
+    else if ( this.state.flag === 2 ) {
+      box_state = <i className="fas fa-question"/>;
+    }
+
     return (
-      <button className="grid-item">{this.props.state === 0 ? "" : this.props.state}</button>
+      <button className={"grid-item " + (this.state.show ? "active" : "")}
+        onClick={this.handleClick}
+        onContextMenu={this.contextHandleClick}>
+        {this.state.flag !== 0 && box_state}
+        {this.state.flag === 0 && this.state.show && (this.props.state === 0 ? "" : box_state)}
+      </button>
     );
   }
 }
@@ -18,4 +64,5 @@ Box.propTypes = {
   state: PropTypes.any.isRequired,
   row: PropTypes.number.isRequired,
   col: PropTypes.number.isRequired,
+  boxClickHandle: PropTypes.func.isRequired,
 };
